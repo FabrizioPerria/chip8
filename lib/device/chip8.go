f4270dacc58cc9b9aabfcfa008cca263d5f3e50a
Fabrizio Perria <fabrizio.perria@gmail.com>
2025-03-08 18:44:35 +0100

    Run first splash

--- lib/device/chip8.go
+++ lib/device/chip8.go
@@ -1,6 +1,7 @@
 package device
 
 import (
+	"fmt"
 	"os"
 	"time"
 )
@@ -69,30 +70,14 @@ type Chip8 struct {
 
 func (c *Chip8) Init() {
 	c.pc = memoryStart
-	c.shouldDraw = true
 	c.beep = func() {}
 
-	for i := range c.memory {
-		c.memory[i] = 0
-	}
+	c.memory = [memorySize]byte{}
+	c.stack = [16]uint16{}
+	c.V = [16]byte{}
+	c.key = [16]byte{}
 
-	for i := range c.stack {
-		c.stack[i] = 0
-	}
-
-	for i := range c.V {
-		c.V[i] = 0
-	}
-
-	for i := range c.key {
-		c.key[i] = 0
-	}
-
-	for i := range c.display {
-		for j := range c.display[i] {
-			c.display[i][j] = 0
-		}
-	}
+	c.clearDisplay()
 
 	copy(c.memory[memoryFontset:], fontset[:])
 
@@ -100,6 +85,11 @@ func (c *Chip8) Init() {
 	c.clock.lastTimerTick = time.Now()
 	c.clock.cycleDelay = time.Second / cycleFrequency
 	c.clock.timerDelay = time.Second / timerFrequency
+}
+
+func (c *Chip8) clearDisplay() {
+	c.display = [DisplayWidth][DisplayHeight]byte{}
+	c.shouldDraw = true
 }
 
 func (c *Chip8) LoadFile(filename string) {
@@ -141,17 +131,18 @@ func (c *Chip8) Step() {
 	now := time.Now()
 	if now.Sub(c.clock.lastCycle) >= c.clock.cycleDelay {
 		c.fetch()
-		c.display[c.currentPixel.x][c.currentPixel.y] = 0
-		c.currentPixel.x += 1
-		if c.currentPixel.x == 64 {
-			c.currentPixel.x = 0
-			c.currentPixel.y += 1
-			if c.currentPixel.y == 32 {
-				c.currentPixel.y = 0
-			}
-		}
-		c.display[c.currentPixel.x][c.currentPixel.y] = 1
-		c.shouldDraw = true
+		c.decode()
+		// c.display[c.currentPixel.x][c.currentPixel.y] = 0
+		// c.currentPixel.x += 1
+		// if c.currentPixel.x == 64 {
+		// 	c.currentPixel.x = 0
+		// 	c.currentPixel.y += 1
+		// 	if c.currentPixel.y == 32 {
+		// 		c.currentPixel.y = 0
+		// 	}
+		// }
+		// c.display[c.currentPixel.x][c.currentPixel.y] = 1
+		// c.shouldDraw = true
 		c.clock.lastCycle = now
 	}
 	c.updateTimers(&now)
@@ -169,6 +160,7 @@ func (c *Chip8) updateTimers(now *time.Time) {
 	}
 }
 
+// 0000 0000 1110 0000
 func (c *Chip8) fetch() {
 	c.oc = uint16(c.memory[c.pc])<<8 | uint16(c.memory[c.pc+1])
 	c.pc += 2
@@ -176,11 +168,67 @@ func (c *Chip8) fetch() {
 
 func (c *Chip8) decode() {
 	opcode_type := (c.oc & 0xF000) >> 12
-	// x := (c.oc & 0x0F00) >> 8
-	// y := (c.oc & 0x00F0) >> 4
-	// n := c.oc & 0x000F
-	// nn := c.oc & 0x00FF
-	// nnn := c.oc & 0x0FFF
+	x := (c.oc & 0x0F00) >> 8
+	y := (c.oc & 0x00F0) >> 4
+	n := c.oc & 0x000F
+	nn := c.oc & 0x00FF
+	nnn := c.oc & 0x0FFF
+	fmt.Println(hex2str(c.oc))
 	switch opcode_type {
+	case 0x0:
+		switch nn {
+		case 0xE0:
+			// 00E0 - CLS
+			c.clearDisplay()
+		case 0xEE:
+			// 00EE - RET
+			c.pc = c.stack[c.sp]
+			c.sp--
+		default:
+			// 0nnn - SYS addr
+			c.pc = nnn
+		}
+	case 0x1:
+		// 1nnn - JP addr
+		c.pc = nnn
+	case 0x6:
+		// 6xkk - LD Vx, byte
+		c.V[x] = byte(nn)
+
+	case 0xA:
+		// Annn - LD I, addr
+		c.I = nnn
+
+	case 0xD:
+		// Dxyn - DRW Vx, Vy, nibble
+		sprite := c.memory[c.I : c.I+n]
+		x := int(c.V[x])
+		y := int(c.V[y])
+		c.drawSprite(x, y, sprite)
+
+	default:
+		panic("Unknown opcode - " + hex2str(opcode_type))
 	}
+}
+
+func (c *Chip8) drawSprite(x, y int, sprite []byte) {
+	c.V[0xF] = 0
+	for j := range sprite {
+		for i := range 8 {
+			spritePixel := (sprite[j] & (0x80 >> uint(i)))
+			if spritePixel != 0 {
+				xCoord := (x + i) % DisplayWidth
+				yCoord := (y + j) % DisplayHeight
+				if c.display[xCoord][yCoord] == 1 {
+					c.V[0xF] = 1
+				}
+				c.display[xCoord][yCoord] ^= 1
+				c.shouldDraw = true
+			}
+		}
+	}
+}
+
+func hex2str(h uint16) string {
+	return fmt.Sprintf("%04X", h)
 }
